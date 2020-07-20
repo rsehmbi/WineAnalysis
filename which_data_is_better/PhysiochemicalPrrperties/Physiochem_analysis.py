faa89d5cabca358b245ed39ee5339ef9e5b74f8b
rsehmbi <rsehmbi@sfu.ca>
2020-07-20 12:54:54 -0700

    scatter plots all the columns of the red and white wine dataframe

--- which_data_is_better/PhysiochemicalPrrperties/Physiochem_analysis.py
+++ which_data_is_better/PhysiochemicalPrrperties/Physiochem_analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Loading the data set in pandas Dataframe
 red_wine_data = pd.read_csv("wineQualityReds.csv")
@@ -64,6 +65,38 @@ print("--- Modified Column Names of White Wine Data Set ---")
 for column_name in white_wine_data.columns:
     print(column_name, end=' ')
 print("\n")
+
+# Next Step
+# Looking at the scatter plot of the data set to observe the general trends and see if we have any outliers
+
+# Plots for Red Wine data set and description of every column of dataframe
+print("-------------------Description of Read Wine Data Set-----------------------------\n")
+for column_name in red_wine_data.columns:
+    print(column_name)
+    print(red_wine_data[column_name].describe())
+    plt.scatter(red_wine_data.Wine_Number,
+                red_wine_data[column_name], alpha=0.5)
+    plt.xlabel("Wine Number")
+    plt.ylabel(column_name)
+    plt.savefig('RedWineScatterPlots/{}.png'.format(column_name))
+    # Clears the old plot so plt don't overwrite
+    plt.clf()
+    print('\n')
+
+print('\n')
+# Plots for White Wine data set and description of every column of dataframe
+print("---------------------Description of White Wine Data Set--------------------------\n")
+for column_name in white_wine_data.columns:
+    print(column_name)
+    print(white_wine_data[column_name].describe())
+    plt.scatter(white_wine_data.Wine_Number,
+                white_wine_data[column_name], alpha=0.5)
+    plt.xlabel("Wine Number")
+    plt.ylabel(column_name)
+    plt.savefig('WhiteWineScatterPlots/{}.png'.format(column_name))
+    # Clears the old plot so plt don't overwrite
+    plt.clf()
+    print('\n')
 
 # print("Description of acidity")
 # print(red_wine_data['fixed.acidity'].describe())
