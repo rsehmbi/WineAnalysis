f2ba859c7afbebc743e2730c703e25f81cd128d9
Kevin Park <kevinpark@Kevins-iMac.hitronhub.home>
2020-07-20 12:42:37 -0700

    Replace NaN with zeros

--- scores_by_types/clean_data.py
+++ scores_by_types/clean_data.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 
 
+def replace_nan(data):
+	data = data.fillna(0)
+	return data
+
+
 def get_counts_by_points(data):
 	points = data.apply(lambda x: x.value_counts(), axis=0)
+	points = replace_nan(points)
 	return points
