650f34691d1c4b279a8fc43dce4769ce2a59f366
rsehmbi <rsehmbi@sfu.ca>
2020-08-03 10:25:42 -0700

    Starts the Locations Analysis of wine

---
+++ which_data_is_better/LocationReviews/Location_analysis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+
+def main():
+    print("****** Welcome to Location Analysis of Red and White Wine ****** \n")
+
+
+if __name__ == "__main__":
+    main()
