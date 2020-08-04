d7454312a8a83b91146fa077117b1168bc5b7e1d
rsehmbi <rsehmbi@sfu.ca>
2020-08-04 15:29:59 -0700

    Refactors the code

--- which_data_is_better/LocationReviews/Transform_Load_Analysis.py
+++ which_data_is_better/LocationReviews/Transform_Load_Analysis.py
@@ -21,10 +21,10 @@ def loaddatasets(filepath):
     return wine_dataset
 
 
-def printbarplot(df):
-    grouped_countries_points = df.groupby(
+def printbarplot(wine_df):
+    grouped_countries_points = wine_df.groupby(
         ["country"], as_index=False)['points'].mean()
-    grouped_countries_price = df.groupby(
+    grouped_countries_price = wine_df.groupby(
         ["country"], as_index=False)['price'].mean()
 
     plt.barh(grouped_countries_points.points,
@@ -44,125 +44,92 @@ def printbarplot(df):
     plt.clf()
 
 
-def Analysis1(df):
+def DecisionTree(X_train, y_train, X_valid, y_valid):
+    model = DecisionTreeClassifier(max_depth=30)
+    model.fit(X_train, y_train)
+    print("Score with Decision Tree Classifier" +
+          str(model.score(X_valid, y_valid)))
+
+
+def GB(X_train, y_train, X_valid, y_valid):
+    modelGB = GradientBoostingClassifier(n_estimators=50,
+                                         max_depth=50, min_samples_leaf=0.1)
+    modelGB.fit(X_train, y_train)
+    print("Score with Gradient Boosting"+str(modelGB.score(X_valid, y_valid)))
+
+
+def KNN(X_train, y_train, X_valid, y_valid):
+    modelKNN = KNeighborsClassifier(n_neighbors=1)
+    modelKNN.fit(X_train, y_train)
+    print(modelKNN.score(X_valid, y_valid))
+
+
+def WithoutRegion2Analysis(wine_df):
     # Since there are limited number of countries, let's look at avg of each country's points and plot a graph for visualization
-    printbarplot(df)
+    printbarplot(wine_df)
 
-    country_, country_unique_values = pd.factorize(df.loc[:, 'country'])
+    country_, country_unique_values = pd.factorize(wine_df.loc[:, 'country'])
     designation_, country_unique_values = pd.factorize(
-        df.loc[:, 'designation'])
-    price_, price_unique_values = pd.factorize(df.loc[:, 'price'])
+        wine_df.loc[:, 'designation'])
+    price_, price_unique_values = pd.factorize(wine_df.loc[:, 'price'])
     province_, province_unique_values = pd.factorize(
-        df.loc[:, 'province'])
-    region_1_, region_unique_values = pd.factorize(df.loc[:, 'region_1'])
+        wine_df.loc[:, 'province'])
+    region_1_, region_unique_values = pd.factorize(wine_df.loc[:, 'region_1'])
     winery_, winery_unique_values = pd.factorize(
-        df.loc[:, 'winery'])
-
-    df['country_'] = country_
-    df['designation_'] = designation_
-    df['price_'] = price_
-    df['province_'] = province_
-    df['winery_'] = winery_
-    df['region_'] = region_1_
-
-    X = df[['country_', 'designation_', 'province_', 'region_', 'winery_']].values
-
+        wine_df.loc[:, 'winery'])
     points_, points_unique_values = pd.factorize(
-        df.loc[:, 'points'])
-    df['points_'] = points_
-    y = df['points_'].values
+        wine_df.loc[:, 'points'])
+
+    wine_df['country_'] = country_
+    wine_df['designation_'] = designation_
+    wine_df['price_'] = price_
+    wine_df['province_'] = province_
+    wine_df['winery_'] = winery_
+    wine_df['region_'] = region_1_
+    wine_df['points_'] = points_
+
+    X = wine_df[['country_', 'designation_',
+                 'province_', 'region_', 'winery_']].values
+    y = wine_df['points_'].values
 
     X_train, X_valid, y_train, y_valid = train_test_split(X, y)
 
-    model = DecisionTreeClassifier(max_depth=30)
-    model.fit(X_train, y_train)
-    print(model.score(X_train, y_train))
-    print(model.score(X_valid, y_valid))
-
-    modelGB = GradientBoostingClassifier(n_estimators=50,
-                                         max_depth=50, min_samples_leaf=0.1)
-    modelGB.fit(X_train, y_train)
-    print(modelGB.score(X_train, y_train))
-    print(modelGB.score(X_valid, y_valid))
-
-    modelRF = RandomForestClassifier(n_estimators=100,
-                                     max_depth=30, min_samples_leaf=30)
-
-    modelRF.fit(X_train, y_train)
-    print(modelRF.score(X_train, y_train))
-    print(modelRF.score(X_valid, y_valid))
-
-    SVC_model = make_pipeline(
-        StandardScaler(),
-        SVC(kernel='linear', C=2.0)
-    )
-
-    SVC_model.fit(X_train, y_train)
-    print(SVC_model.score(X_valid, y_valid))
-
-    modelKNN = KNeighborsClassifier(n_neighbors=1)
-    modelKNN.fit(X_train, y_train)
-    print(modelKNN.score(X_valid, y_valid))
+    DecisionTree(X_train, X_valid, y_train, y_valid)
+    GB(X_train, X_valid, y_train, y_valid)
+    KNN(X_train, X_valid, y_train, y_valid)
 
 
-def Analysis2(df):
-    country_, country_unique_values = pd.factorize(df.loc[:, 'country'])
+def WithRegionAnalysis(wine_df):
+    country_, country_unique_values = pd.factorize(wine_df.loc[:, 'country'])
     designation_, country_unique_values = pd.factorize(
-        df.loc[:, 'designation'])
-    price_, price_unique_values = pd.factorize(df.loc[:, 'price'])
+        wine_df.loc[:, 'designation'])
+    price_, price_unique_values = pd.factorize(wine_df.loc[:, 'price'])
     province_, province_unique_values = pd.factorize(
-        df.loc[:, 'province'])
-    region_1_, region_unique_values = pd.factorize(df.loc[:, 'region_1'])
-    region_2_, region_unique_values = pd.factorize(df.loc[:, 'region_2'])
+        wine_df.loc[:, 'province'])
+    region_1_, region_unique_values = pd.factorize(wine_df.loc[:, 'region_1'])
+    region_2_, region_unique_values = pd.factorize(wine_df.loc[:, 'region_2'])
     winery_, winery_unique_values = pd.factorize(
-        df.loc[:, 'winery'])
+        wine_df.loc[:, 'winery'])
+    points_, points_unique_values = pd.factorize(wine_df.loc[:, 'points'])
 
-    df['country_'] = country_
-    df['designation_'] = designation_
-    df['price_'] = price_
-    df['province_'] = province_
-    df['winery_'] = winery_
-    df['region1_'] = region_1_
-    df['region2_'] = region_2_
+    wine_df['country_'] = country_
+    wine_df['designation_'] = designation_
+    wine_df['price_'] = price_
+    wine_df['province_'] = province_
+    wine_df['winery_'] = winery_
+    wine_df['region1_'] = region_1_
+    wine_df['region2_'] = region_2_
+    wine_df['points_'] = points_
 
-    X = df[['country_', 'designation_',
-            'province_', 'region1_', 'region2_', 'winery_']].values
-
-    points_, points_unique_values = pd.factorize(df.loc[:, 'points'])
-    df['points_'] = points_
-    y = df['points_'].values
+    X = wine_df[['country_', 'designation_',
+                 'province_', 'region1_', 'region2_', 'winery_']].values
+    y = wine_df['points_'].values
 
     X_train, X_valid, y_train, y_valid = train_test_split(X, y)
 
-    model = DecisionTreeClassifier(max_depth=30)
-    model.fit(X_train, y_train)
-    print(model.score(X_train, y_train))
-    print(model.score(X_valid, y_valid))
-
-    modelGB = GradientBoostingClassifier(n_estimators=50,
-                                         max_depth=50, min_samples_leaf=0.1)
-    modelGB.fit(X_train, y_train)
-    print(modelGB.score(X_train, y_train))
-    print(modelGB.score(X_valid, y_valid))
-
-    modelRF = RandomForestClassifier(n_estimators=100,
-                                     max_depth=30, min_samples_leaf=30)
-
-    modelRF.fit(X_train, y_train)
-    print(modelRF.score(X_train, y_train))
-    print(modelRF.score(X_valid, y_valid))
-
-    SVC_model = make_pipeline(
-        StandardScaler(),
-        SVC(kernel='linear', C=2.0)
-    )
-
-    SVC_model.fit(X_train, y_train)
-    print(SVC_model.score(X_valid, y_valid))
-
-    modelKNN = KNeighborsClassifier(n_neighbors=1)
-    modelKNN.fit(X_train, y_train)
-    print(modelKNN.score(X_valid, y_valid))
+    DecisionTree(X_train, X_valid, y_train, y_valid)
+    GB(X_train, X_valid, y_train, y_valid)
+    KNN(X_train, X_valid, y_train, y_valid)
 
 
 def main():
@@ -170,8 +137,8 @@ def main():
     wine_dataset_without_region = loaddatasets("DataWithoutRegion2")
     wine_dataset_with_region = loaddatasets("DatawithRegion2")
 
-    Analyze_withoutRegion = Analysis1(wine_dataset_without_region)
-    Analyze_withRegion = Analysis2(wine_dataset_with_region)
+    Analyze_withoutRegion = WithoutRegion2Analysis(wine_dataset_without_region)
+    Analyze_withRegion = WithRegionAnalysis(wine_dataset_with_region)
 
 
 if __name__ == "__main__":
