df49dc705b48fa5930057ad2193ff88ddc225a2d
Kevin Park <kevinpark@Kevins-iMac.hitronhub.home>
2020-08-09 02:24:37 -0700

    Added vivino_scrape.py

---
+++ vivino_scrape/vivino_scrape_sel.py
@@ -0,0 +1,36 @@
+import time
+from selenium import webdriver
+import copy
+from bs4 import BeautifulSoup
+import csv
+
+
+url = 'https://www.vivino.com/explore?e=eJzLLbI1VMvNzLM1UMtNrLA1NTBQS660dXZUSwYSLmoFQNn0NNuyxKLM1JLEHLX8JNuixJLMvPTi-MSy1KLE9FS1fNuU1OJktfKS6FigYjBlBACsPByG'
+driver = webdriver.Firefox()
+# driver = webdriver.Chrome()
+driver.get(url)
+prev_height = driver.execute_script("return document.body.scrollHeight")
+fields = ['title', 'region', 'ratings', 'number of ratings']
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == prev_height:
+        break
+    prev_height = new_height
+html = BeautifulSoup(driver.page_source, 'lxml')
+div = html.find("div", {"class": "explorerPage__results--3wqLw"})
+rows = html.find_all("div", {"class": "explorerCard__explorerCard--3Q7_0"})
+wine = {"title": None, "region": None, "ratings": None, "number of ratings": None}
+
+with open("vivino_data.csv", "w") as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(fields)
+    for r in rows:
+        row = copy.copy(wine)
+        row['title'] = r.find("span", {"class": "vintageTitle__wine--U7t9G"}).text
+        row['region'] = r.find("div", {"class": "vintageLocation__vintageLocation--1DF0p"}).text
+        row['ratings'] = r.find("div", {"class": "vivinoRatingWide__averageValue--1zL_5"}).text
+        row['number of ratings'] = int(r.find("div", {"class": "vivinoRatingWide__basedOn--s6y0t"}).text.split()[0])
+        csv_writer.writerow([row['title'], row['region'], row['ratings'], row['number of ratings']])
+        print("{},{},{},{}".format(row['title'], row['region'], row['ratings'], row['number of ratings']))
