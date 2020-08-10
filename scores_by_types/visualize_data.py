5efd4d9b631bdf19dac02a28d213eacb2369ab5d
Kevin Park <kevinpark@Kevins-iMac.hitronhub.home>
2020-08-09 20:48:47 -0700

    Added individual bar graphs and violin plot

--- scores_by_types/visualize_data.py
+++ scores_by_types/visualize_data.py
@@ -1,11 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 
-def get_bar(points, range_min, range_max):
+def plot_bar_counts(points, range_min, range_max):
+	sns.set()
 	scores = np.arange(range_min, range_max+1)
-	width=0.2
+	width = 0.2
 	plt.figure(figsize=(15,10))
 	plt.bar(scores-(width*2), points['Bordeaux-style Red Blend'],align='center', width=width, color='r', edgecolor='white')
 	plt.bar(scores-width, points['Cabernet Sauvignon'], align='center', width=width, color='b', edgecolor='white')
@@ -17,4 +19,30 @@ def get_bar(points, range_min, range_max):
 	plt.xlabel('Scores')
 	plt.ylabel('Count')
 	plt.show()
-	# plt.savefig('./outputs/wine_scores_by_types.png')
+
+def plot_violin(points):
+	sns.set()
+	ax = sns.violinplot(data=points)
+	plt.title('Kernel Density Estimate with Box Plot of Wine Types Scores', fontsize=19)
+	plt.xlabel('Wine Types', fontsize=15)
+	plt.ylabel('Score', fontsize=15)
+	plt.show()
+
+def plot_bar_indi(points, range_min, range_max):
+	sns.set()
+	scores = np.arange(range_min, range_max+1)
+	fig, axes = plt.subplots(nrows=3, ncols=2)
+	axes[0, 0].bar(scores, points['Bordeaux-style Red Blend'], color='r')
+	axes[0, 0].set(title='Bordeaux-style Red Blend')
+	axes[0, 1].bar(scores, points['Cabernet Sauvignon'], color='b')
+	axes[0, 1].set(title='Cabernet Sauvignon')
+	axes[1, 0].bar(scores, points['Chardonnay'], color='g')
+	axes[1, 0].set(title='Chardonnay')
+	axes[1, 1].bar(scores, points['Pinot Noir'], color='m')
+	axes[1, 1].set(title='Pinot Noir')
+	axes[2, 0].bar(scores, points['Red Blend'], color='y')
+	axes[2, 0].set(title='Red Blend')
+	fig.delaxes(axes[2, 1])
+	for ax in axes.flat:
+		ax.set(xlabel='Score', ylabel='Count')
+	plt.show()
