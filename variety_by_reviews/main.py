10b633980413aa2af1a9320b4e2e59f7ef3aa521
Kevin Park <kevinpark@Kevins-iMac.hitronhub.home>
2020-08-10 14:02:04 -0700

    Description of the program in README

--- variety_by_reviews/main.py
+++ variety_by_reviews/main.py
@@ -4,7 +4,6 @@ import clean_data as clean
 
 
 def main():
-    # data = pd.read_csv('./data/winemag-data-130k-v2.csv')
     data = pd.read_csv('./data/1442_8172_compressed_winemag-data-130k-v2.csv.zip')
     clean.clean_description(data)
 
